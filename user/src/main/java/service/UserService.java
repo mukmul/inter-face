d30f368ce3fbd686e86fe4bbfa51a1d4affb7264
JinjinBread <pge0720@naver.com>
2024-12-12 16:56:51 +0900

    feat: add create, read, delete method at UserService

---
+++ user/src/main/java/service/UserService.java
@@ -0,0 +1,46 @@
+package service;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import mukmul.inter_face.user.UserEntity;
+import mukmul.inter_face.user.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Transactional
+    public UserEntity save(UserEntity userEntity) {
+        return userRepository.save(userEntity);
+    }
+
+    /**
+     * 1. DTO 생성 후 반환 타입 변경
+     * 2. 커스텀 에러 생성 후 변경
+     */
+    public UserEntity findById(Long userId) {
+        return userRepository.findById(userId)
+                .orElseThrow(() -> new RuntimeException("찾을 수 없는 사용자입니다."));
+    }
+
+    public UserEntity findByEmail(String userEmail) {
+        return userRepository.findByUserEmail(userEmail)
+                .orElseThrow(() -> new RuntimeException("찾을 수 없는 사용자입니다."));
+    }
+
+    @Transactional
+    public UserEntity delete(Long userId) {
+        UserEntity findUser = findById(userId);
+
+        if (findUser.getDeletedAt() != null) {
+            throw new RuntimeException("이미 삭제된 사용자입니다.");
+        }
+
+        findUser.delete();
+        return findUser;
+    }
+
+}

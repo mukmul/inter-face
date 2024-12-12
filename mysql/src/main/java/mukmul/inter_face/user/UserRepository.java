fa138adcd8902f0998911fa22c10120db7845304
JinjinBread <pge0720@naver.com>
2024-12-12 16:41:37 +0900

    feat: add findByUserEmail at UserRepository

--- mysql/src/main/java/mukmul/inter_face/user/UserRepository.java
+++ mysql/src/main/java/mukmul/inter_face/user/UserRepository.java
@@ -3,6 +3,10 @@ package mukmul.inter_face.user;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
+
+    Optional<UserEntity> findByUserEmail(String userEmail);
 }
